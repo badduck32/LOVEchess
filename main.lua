d1c3a002e066e807fa3d48f9c3cbe0420ae1d9f8
Badduck <badduckssb@gmail.com>
2021-11-22 16:40:03 +0100

    buggy spaghetti

--- main.lua
+++ main.lua
@@ -1,9 +1,22 @@
 function love.load()
-	love.graphics.setMode(800, 800)
+	love.window.setMode(800, 800, {resizable = false} )
+	xOffs = 0
+	yOffs = 100
 end
 
 function love.update(dt)
-	for v, i in 
+	--[[for i = 1,64
+		love.graphics.rectangle("fill", )
+	end--]]
 end
 
 function love.draw()
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.rectangle("fill", xOffs, yOffs, 400, 400)
+	love.graphics.setColor(0, 0, 0)
+	for x = 0,7 do
+		if x % 2 == 0 then
+			love.graphics.rectangle("fill", x * 50 + xOffs, yOffs, 50, 50)
+		end
+	end
+end
