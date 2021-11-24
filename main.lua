4cb655deb1502dd893afc83d4a7ca23a82b834ac
Badduck <badduckssb@gmail.com>
2021-11-25 00:33:21 +0100

    changed a bunch

--- main.lua
+++ main.lua
@@ -1,22 +1,33 @@
 function love.load()
 	love.window.setMode(800, 800, {resizable = false} )
-	xOffs = 0
-	yOffs = 100
+	xOffs = 5
+	yOffs = 250
+	--if white has to plat turn is 0, if black1
+	turn = 0
+	all = love.graphics.newImage("chess pieces.png")
+	wpawn = love.graphics.newQuad(1000, 0, 200, 200, all:getDimensions())
 end
 
 function love.update(dt)
-	--[[for i = 1,64
-		love.graphics.rectangle("fill", )
-	end--]]
+	
 end
 
 function love.draw()
-	love.graphics.setColor(1, 1, 1)
+	--draws board
+	love.graphics.setColor(0.95, 0.75, 0.6)
 	love.graphics.rectangle("fill", xOffs, yOffs, 400, 400)
-	love.graphics.setColor(0, 0, 0)
+	love.graphics.setColor(0.875, 0.6, 0.5)
 	for x = 0,7 do
-		if x % 2 == 0 then
-			love.graphics.rectangle("fill", x * 50 + xOffs, yOffs, 50, 50)
+		for y = 0, 7 do
+			if (x + y) % 2 == 0 then
+				love.graphics.rectangle("fill", x * 50 + xOffs, y * 50 + yOffs, 50, 50)
+			end
 		end
+	end
+	
+	love.graphics.setColor(1, 1, 1)
+	--draw white pawns
+	for i = 0, 7 do
+		love.graphics.draw(all, wpawn, xOffs + i * 50, yOffs + 6 * 50, turn * 180, 0.25, 0.25)
 	end
 end
