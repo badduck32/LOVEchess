c7c217bbffe57e2532416fa1e1bdc81fee4bef14
Badduck <badduckssb@gmail.com>
2021-11-27 21:35:13 +0100

    blavka

--- main.lua
+++ main.lua
@@ -1,15 +1,16 @@
 function love.load()
-	love.window.setMode(411, 800, {resizable = false} )
+	love.window.setMode(800, 800, {resizable = false} )
 	xOffs = 5
 	yOffs = 250
 	--if white has to plat turn is 0, if black1
-	turn = 1
+	turn = 0
 	all = love.graphics.newImage("chess pieces.png")
 	wpawn = love.graphics.newQuad(1000, 0, 200, 200, all:getDimensions())
+	Piece = {x, y}
 end
 
 function love.update(dt)
-	--update
+	
 end
 
 function love.draw()
@@ -28,6 +29,6 @@ function love.draw()
 	love.graphics.setColor(1, 1, 1)
 	--draw white pawns
 	for i = 0, 7 do
-		love.graphics.draw(all, wpawn, xOffs + i * 50, yOffs + 6 * 50, (turn)*(math.pi), 0.25, 0.25)
+		love.graphics.draw(all, wpawn, xOffs + i * 50, yOffs + 6 * 50, turn * 180, 0.25, 0.25)
 	end
 end
