49555640c546caa032a2d9b7949970572b1736a1
Badduck <badduckssb@gmail.com>
2021-12-08 00:18:07 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,29 +8,29 @@ function love.load()
 	types = {love.graphics.newQuad(0, 0, 200, 200, all:getDimensions()), love.graphics.newQuad(200, 0, 200, 200, all:getDimensions()),
 	love.graphics.newQuad(400, 0, 200, 200, all:getDimensions()), love.graphics.newQuad(600, 0, 200, 200, all:getDimensions()),
 	love.graphics.newQuad(800, 0, 200, 200, all:getDimensions()), love.graphics.newQuad(1000, 0, 200, 200, all:getDimensions()) }
-	Piece = {x = 0, y = 0, dead = false, type = 5}
-	--types: 0 = king, 1 = queen, 2 = bishop, 3 = knight, 4 = rook, 5 = pawn
+	Piece = {x = 0, y = 0, dead = false, type = 0}
+	--types: 1 = king, 2 = queen, 3 = bishop, 4 = knight, 5 = rook, 6 = pawn
 
-function Piece:create (o)
-  o.parent = self
-  return o
-end
+	function Piece:create (o)
+  		o.parent = self
+  		return o
+	end
 
-function Piece:move (p)
-  self.x = self.x + p.x
-  self.y = self.y + p.y
-end
+	function Piece:move (p)
+  		self.x = self.x + p.x
+  		self.y = self.y + p.y
+	end
 
---
--- creating points
---
-p1 = Piece:create{x = 2, y = 2}
-p2 = Piece:create{x = 10}  -- y will be inherited until it is set
+	--
+	-- creating points
+	--
+	p1 = Piece:create{x = 2, y = 2, dead = false, type = 2}
+	p2 = Piece:create{x = 10}  -- y will be inherited until it is set
 
---
--- example of a method invocation
---
---p1:move(p2)
+	--
+	-- example of a method invocation
+	--
+	--p1:move(p2)
 end
 
 function love.update(dt)
@@ -40,6 +40,7 @@ end
 function love.draw()
 	--draws board
 	love.graphics.setColor(0.95, 0.75, 0.6)
+	--love.graphics.print(p1.type + "t", 10, 10)
 	love.graphics.rectangle("fill", xOffs, yOffs, 400, 400)
 	love.graphics.setColor(0.875, 0.6, 0.5)
 	for x = 0,7 do
@@ -53,6 +54,6 @@ function love.draw()
 	love.graphics.setColor(1, 1, 1)
 	--draw white pawns
 	for i = 0, 7 do
-		love.graphics.draw(all, types[6], xOffs + p1.x * 50, yOffs + p1.y * 50, --[[turn * 180--]]0, 0.25, 0.25)
+		love.graphics.draw(all, types[p1.type], xOffs + p1.x * 50, yOffs + p1.y * 50, --[[turn * 180--]]0, 0.25, 0.25)
 	end
 end
