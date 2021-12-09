da88202807007c11d298c6964c555488cab9e60e
Badduck <badduckssb@gmail.com>
2021-12-09 14:33:06 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -37,28 +37,28 @@ function love.load()
 	function createPieces (white)
 		if white then
 			offset = 0
-			multiplier = 1
+			mult = 1
 		else
 			offset = 7
-			multiplier = -1
+			mult = -1
 		end
 		arr = {}
 		for i = 1, 8 do
-			arr[i] = Piece:create{x = i - 1, y = offset + multiplier * 6, dead = false, type = 6, white = white}
+			arr[i] = Piece:create{x = i - 1, y = offset + mult * 6, dead = false, type = 6, white = white}
 		end
 		--rooks
-		arr[9] = Piece:create{x = 0, y = offset + multiplier * 7, dead = false, type = 5, white = white}
-		arr[10] = Piece:create{x = 7, y = offset + multiplier * 7, dead = false, type = 5, white = white}
+		arr[9] = Piece:create{x = 0, y = offset + mult * 7, dead = false, type = 5, white = white}
+		arr[10] = Piece:create{x = 7, y = offset + mult * 7, dead = false, type = 5, white = white}
 		--knights
-		arr[11] = Piece:create{x = 1, y = offset + multiplier * 7, dead = false, type = 4, white = white}
-		arr[12] = Piece:create{x = 6, y = offset + multiplier * 7, dead = false, type = 4, white = white}
+		arr[11] = Piece:create{x = 1, y = offset + mult * 7, dead = false, type = 4, white = white}
+		arr[12] = Piece:create{x = 6, y = offset + mult * 7, dead = false, type = 4, white = white}
 		--bishops
-		arr[13] = Piece:create{x = 2, y = offset + multiplier * 7, dead = false, type = 3, white = white}
-		arr[14] = Piece:create{x = 5, y = offset + multiplier * 7, dead = false, type = 3, white = white}
+		arr[13] = Piece:create{x = 2, y = offset + mult * 7, dead = false, type = 3, white = white}
+		arr[14] = Piece:create{x = 5, y = offset + mult * 7, dead = false, type = 3, white = white}
 		--queen
-		arr[15] = Piece:create{x = 3, y = offset + multiplier * 7, dead = false, type = 2, white = white}
+		arr[15] = Piece:create{x = 3, y = offset + mult * 7, dead = false, type = 2, white = white}
 		--king
-		arr[16] = Piece:create{x = 4, y = offset + multiplier * 7, dead = false, type = 1, white = white}
+		arr[16] = Piece:create{x = 4, y = offset + mult * 7, dead = false, type = 1, white = white}
 		--1-8: pawns, 9-10: rook, 11-12: knight, 13-14: bishop, 15: queen, 16: king
 		return arr
 	end
@@ -71,14 +71,15 @@ function love.keypressed(key)
 	turn = turn == 0 and 1 or 0
 end
 
+--clicking/touching implementation
 function love.touchpressed(id, x, y, dx, dy, p)
 	clicked(x, y)
 end
-
 function love.mousepressed(x, y, b, isTouch)
 	clicked(x, y)
 end
 
+--returns the piece at specified x and y coordinate (tile coords)
 function pieceAt(x, y)
 	for i, piece in ipairs(wpieces) do 
 		if piece.x == x and piece.y == y then
@@ -90,12 +91,30 @@ function pieceAt(x, y)
 			return piece
 		end
 	end
-
+end
 
 function clicked (fx, fy)
+	highlights = {}
+	--finds which tile was clicked
 	x = math.floor((fx - xOffs) / 50)
 	y = math.floor((fy - yOffs) / 50)
+	--creates first highlight at clicked piece pos
 	highlights[1] = Highlight:create{x = x, y = y}
+	if pieceAt(x, y) ~= nil then
+		createHighlights(pieceAt(x, y))
+	end
+end
+
+function createHighlights(piece)
+	--pawn
+	if piece.type == 6 then 
+		if piece.white then
+			highlights[2] = Highlight:create{x = piece.x, y = piece.y - 1}
+		else 
+			highlights[2] = Highlight:create{x = piece.x, y = piece.y + 1}
+		end
+	end
+
 end
 
 function love.draw()
@@ -128,6 +147,5 @@ function love.draw()
 	drawPieces(wpieces)
 	--draw black pieces
 	drawPieces(bpieces)
-	--love.graphics.print( "Mouse X: ".. mousex .. " Mouse Y: " .. mousey, 10, 50 )
-	
+	love.graphics.print( tostring(txt) )
 end
